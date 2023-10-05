4d5f58bedd94129c3764a1cbe227cd7593cffd55
Patience <ugwupatiencechy@gmail.com>
2023-10-05 03:15:54 -0700

    malloc and free task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**_puts_recursion - recursive puts
+* @s: pointer params 
+*/
+
+void _puts_recursion(char *s);
+
+{
+	if
+	{
+		(*s == '\n');
+		Putchar('\n');
+	}
+	else
+	{
+		_putchar(s*);
+		_puts_recursion(s + 1);
+	}
+}
